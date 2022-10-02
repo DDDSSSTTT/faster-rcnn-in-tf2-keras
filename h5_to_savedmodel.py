6c132d11c7adeac37bcbe55a12052f1ea7e4e6f9
DDDSSSTTT <756487617@qq.com>
2022-10-02 16:54:16 -0700

    update demo images

--- h5_to_savedmodel.py
+++ h5_to_savedmodel.py
@@ -3,13 +3,11 @@ import tensorflow as tf
 from model.faster_rcnn import FasterRCNN
 from data import pascal_custom
 
-
-
 model = FasterRCNN(is_training=True)
-ds_chess = pascal_custom.pascal_voc(is_training=True, use_diff=False)
+ds_chess = pascal_custom.pascal_voc(dir_name='chess_voc', cls_name='_classes.txt', is_training=True, use_diff=False)
 ds_salad = pascal_custom.non_voc_txts(is_training=True, use_diff=False)
 eval_ds_salad = pascal_custom.non_voc_txts(is_training=False, use_diff=False)
-sanity_sample = np.array(ds_salad.get_small_dataset(30))
-_ = model(sanity_sample[0])
-model.load_weights("model/ckpt/faster_rcnn_vgg16.h5")
+# sanity_sample = np.array(ds_salad.get_small_dataset(30))
+# _ = model(sanity_sample[0])
+# model.load_weights("model/ckpt/faster_rcnn_vgg16.h5")
 model.save("custom_faster")
